eba42b89f00a960dc620f8e2314aa850ceb6fe6d
Matt Maher <matt.maher@sony.com>
2023-05-20 08:45:08 -0700

    extended SerpAPIWrapper to grab the link we need

--- ice_breaker.py
+++ ice_breaker.py
@@ -3,17 +3,24 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
 if __name__ == "__main__":
+    linkedin_profile_url = linkedin_lookup_agent("Eden Marco Udemy")
+
     summary_template = """
         given the LinkedIn information {information} about a person I want you to create:
         1. a short summary
         2. two interesting facts about them
     """
-    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    linkedin_json = scrape_linkedin_profile("https://www.linkedin.com/in/elonmusk/")
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url=linkedin_profile_url
+    )
 
-    print(chain.run(information=linkedin_json))
+    print(chain.run(information=linkedin_data))
--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -4,25 +4,36 @@ import requests
 
 # https://nubela.co/proxycurl/linkedin
 
-def scrape_linkedin_profile(url: str):
+
+def scrape_linkedin_profile(linkedin_profile_url: str):
     """Scrape information from LinkedIn profiles.
     Manually scrape the information from the LinkedIn profile page."""
 
-    header_dic = {"Authorization": f"Bearer {os.environ.get('PROXYCURL_API_KEY')}"}
+    # header_dic = {
+    #     "Authorization": f"Bearer {os.environ.get('PROXYCURL_API_KEY')}"}
 
-    # api_endpoint = "https://api.proxycurl.com/linkedin"
-    # response = requests.get(api_endpoint, headers=header_dic, params={"url": url})
+    header_dic = {
+        "Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
+
+    # READ FROM PROXYCURL
+    api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+    response = requests.get(
+        api_endpoint,
+        params={"url": linkedin_profile_url},
+        headers=header_dic
+    )
 
     # READ FROM OUR GIST
-    api_endpoint = "https://gist.githubusercontent.com/bladnman/62f1b4e8bcb9af9639c9eff781112fcb/raw/b5e12cfd4abba268595942a3db9286505b2f59db/musk_linkedin.json"
-    response = requests.get(api_endpoint)
+    # api_endpoint = "https://gist.githubusercontent.com/bladnman/62f1b4e8bcb9af9639c9eff781112fcb/raw/b5e12cfd4abba268595942a3db9286505b2f59db/musk_linkedin.json"
+    # response = requests.get(api_endpoint)
+
     data_json = response.json()
 
     cleaned_json = {
         k: v
         for k, v in data_json.items()
-        if v not in ([], "", None)
-           and k not in ["people_also_viewed", "certifications"]
+        if v not in ([], "", None) and k not in ["people_also_viewed",
+                                                 "certifications"]
     }
     if cleaned_json.get("groups"):
         for group_dict in cleaned_json.get("groups"):
